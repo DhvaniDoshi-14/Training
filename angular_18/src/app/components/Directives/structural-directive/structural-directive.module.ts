da9fc798c4c5f61e8aa5e5484aa2e0638a3bf4cf
DhvaniDoshi-14 <dhvani.doshi@samyak.com>
2025-05-28 12:59:57 +0530

    add structural directive component and routing module

---
+++ angular_18/src/app/components/Directives/structural-directive/structural-directive.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StructuralDirectiveRoutingModule } from './structural-directive-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StructuralDirectiveRoutingModule
+  ]
+})
+export class StructuralDirectiveModule { }
